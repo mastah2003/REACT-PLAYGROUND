ba80186455f85ad3832159324326442505e27d35
mastah2003 <44333580+mastah2003@users.noreply.github.com>
2019-02-27 21:46:57 +0100

    Initial commit.

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div class="wrapper">
+          <div class="box navi greyback">NAVIGATION</div>
+          <div class="box leftBar"></div>
+          <div class="box content greyback">CONTENT<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>Content</div>
+          <div class="box rightBar"></div>
+          <div class="box footer greyback">FOOTER</div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
